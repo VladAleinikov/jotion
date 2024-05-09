b8c28c1b2f86b131dd131daac445356f6d89e4ea
Vladik <vlad.a.aleynikov@gmail.com>
2024-05-09 18:02:15 +0300

    add autherntication

--- app/(landing)/_components/navbar.tsx
+++ app/(landing)/_components/navbar.tsx
@@ -4,9 +4,16 @@ import { UseScrollTop } from "@/hooks/use-scroll-top"
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, SignUpButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
 
 export const Navbar = () => {
       const scrolled = UseScrollTop();
+      const { isAuthenticated, isLoading } = useConvexAuth();
+
       return (
             <div className={
                   cn(
@@ -16,7 +23,36 @@ export const Navbar = () => {
             }>
                   <Logo />
                   <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
-                        <ModeToggle/>
+                        {isLoading && (
+                              <Spinner />
+                        )}
+                        {!isAuthenticated && !isLoading && (
+                              <>
+                                    <SignInButton mode="modal">
+                                          <Button variant="ghost" size="sm">
+                                                Авторизоваться
+                                          </Button>
+                                    </SignInButton>
+                                    <SignUpButton mode="modal">
+                                          <Button size="sm">
+                                                Начни работать с Jotion
+                                          </Button>
+                                    </SignUpButton>
+                              </>
+                        )}
+                        {isAuthenticated && !isLoading && (
+                              <>
+                                    <Button variant="ghost" size="sm" asChild>
+                                          <Link
+                                                href="/documents"
+                                          >
+                                                Войти в Jotion
+                                          </Link>
+                                    </Button>
+                                    <UserButton afterSignOutUrl="/" />
+                              </>
+                        )}
+                        <ModeToggle />
                   </div>
             </div>
       )
